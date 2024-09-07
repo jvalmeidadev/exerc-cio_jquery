8f53c4fc624996d8e17cca9a24e8f97ced31aae4
jvalmeidadev <jvalmeida.dev@outlook.com>
2024-09-07 09:29:15 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    
+    const $novaTarefa = $('#novaTarefa');
+    const $adicionarTarefa = $('#adicionarTarefa');
+    const $listaTarefas = $('#listaTarefas');
+    const $cancelarTarefa = $('#cancelarTarefa');
+
+    
+    $adicionarTarefa.click(function() {
+        const novaTarefa = $novaTarefa.val();
+        $listaTarefas.append(`<li>${novaTarefa}</li>`);
+        $novaTarefa.val('');
+    });
+        
+    $("#cancelarTarefa").click(function(){
+        $listaTarefas.empty(`<li>${listaTarefas}</li>`);
+        $listaTarefas.val('');
+    })
+    
+    
+        
+    
+    
+    
+    $listaTarefas.on('click', 'li', function() {
+        
+        $(this).addClass('concluida');
+    });
+});
+
+
+
+
